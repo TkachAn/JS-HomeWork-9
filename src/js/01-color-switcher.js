6c593654a3a0b2ed02d251fb7c69359e6f49d4bd
TkachAn <4ndrey.tkachenko@gmail.com>
2022-06-16 15:52:35 +0300

    start

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,27 @@
+const background = document.querySelector("body");
+const startBtn = document.querySelector("button[ data-start]");
+const stopBtn = document.querySelector("button[ data-stop]");
+let timer = null;
+startBtn.addEventListener("click", () => {
+	if(timer === null){
+		timer = setInterval(setColor, 1000);
+	}
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timer);
+	console.log(`Interval with id ${timer} has stopped!`); timer = null;
+});
+
+function setColor() {
+	let hex = `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`
+	 background.style = `background-color: ${hex};`
+	console.log("setHex:", hex);
+  return hex;
+	
+}
+function getRandomHex() {
+  return Math.round(Math.random() * 256)
+    .toString(16)
+    .padStart(2, "0");
+}
