95f87fb289d6a1c1a33dec5128555c61f052c58f
TkachAn <4ndrey.tkachenko@gmail.com>
2022-06-20 14:08:23 +0300

    task3_00

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,65 +1,65 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-//Notify.success();Notify.failure();
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-flatpickr("#datetime-picker", {enableTime: true,
-	dateFormat: "Y-m-d H:i",
+flatpickr("#datetime-picker", {
+	enableTime: true, dateFormat: "Y-m-d H:i",
 });
 const Days = 0;
 const Hours = 1;
 const Minutes = 2;
-const Seconds = 3;	 
+const Seconds = 3;	
+
 let times = 0;
 let timerId = null;
 let diffOn = 0;
-const inData = document.querySelector('#datetime-picker');
+
+const inputData = document.querySelector('#datetime-picker');
 const digitVal = document.querySelectorAll('[class="value"]');
-//console.log("digitVal", digitVal[Minutes]);
 const btnStart = document.querySelector('button');
+
 btnStart.setAttribute('disabled', true);
-
-inData.addEventListener("input", () => {
+inputData.addEventListener("input", () => {
 	
-	times = inData.value;
+	times = inputData.value;
 	const realTime = new Date();
-	//console.log("realTime", realTime);
 	const planTime = new Date(times);
-	//console.log("planTime", planTime);
-
+	
 	if (realTime < planTime) {
-		Notify.success(`PUSH START, please!`);// ${planTime} > ${realTime}`);
+		Notify.success(`PUSH START, please!`);
 		const difference = planTime - realTime;
 		btnStart.removeAttribute('disabled');
-		
 	} else {
 		clearInterval(timerId);
 		btnStart.setAttribute('disabled', true);
 		Notify.failure(`Please select a different date ${planTime }`);
 	}
-	//return inData.value;
+	
 });
-//const timerOn = ()
 
 btnStart.addEventListener("click", () => {
 	timerId = setInterval(() => {
-		times = inData.value;
+		times = inputData.value;
 		const realTime = new Date();
-		//console.log("realTime", realTime);
 		const planTime = new Date(times);
 		diffOn = planTime - realTime;
 		if (planTime > realTime) {
-			//console.log(`I love async JS!  ${diffOn}`);
-			digitVal[Days].textContent = new Date(diffOn).getUTCDate() - 1;
-			digitVal[Hours].textContent = new Date(diffOn).getUTCHours();
-			digitVal[Minutes].textContent = new Date(diffOn).getUTCMinutes();
-			digitVal[Seconds].textContent = new Date(diffOn).getUTCSeconds();
-		} else { 
+			btnStart.setAttribute('disabled', true);
+
+			second = new Date(diffOn).getUTCSeconds();
+			minute = new Date(diffOn).getUTCMinutes();
+			hour = new Date(diffOn).getUTCHours();
+			day = new Date(diffOn).getUTCDate() - 1;
+
+			digitVal[Days].textContent = (day < 10) ? '0' + day : day;
+			digitVal[Hours].textContent = (hour < 10) ? '0' + hour : hour;
+			digitVal[Minutes].textContent = (minute < 10) ? '0' + minute : minute;
+			digitVal[Seconds].textContent = (second < 10) ? '0' + second : second;
+		} else {
 			clearInterval(timerId);
-			Notify.success(`${diffOn} Hooray! earned!`);
+			Notify.success(`$Hooray! earned!`);
 		}
   }, 1000);
 });
-
 
 
 // digitVal[Days].textContent    = new Date(difference).getUTCDate()-1;
