487aad33e7004d1421308cc8a48c6636eed59f3a
TkachAn <4ndrey.tkachenko@gmail.com>
2022-06-23 10:45:50 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -25,21 +25,21 @@ form.addEventListener("submit", (event) => {
 });//.
 const createPromise = (delay, position,stepDelay )=> {
   return new Promise((resolve, reject) => {
-    console.log(`delay ${stepDelay}`);
+    //console.log(`delay ${stepDelay}`);
 	let cnt = 1;
 		// const av = amount.value;
 		// const sv = step.value;
-	  Notify.success(`First delay ${delay}`);
+	  Notify.success(`Fulfilled promise ${cnt} in ${delay}`);
 		let counter = setInterval(() => {
 			const shouldResolve = Math.random() > 0.3;
 			if (shouldResolve) {
 				resolve(`Fulfilled promise ${position} in ${stepDelay}`);
-				console.log("resolve step:", cnt, " ms:", cnt * stepDelay+Number(delay));
-				Notify.success(`Fulfilled promise ${cnt} in ${cnt * stepDelay+Number(delay)}`);	// Fulfill
+				//console.log("resolve step:", cnt, " ms:", cnt * stepDelay+Number(delay));
+				Notify.success(`Fulfilled promise ${cnt+1} in ${cnt * stepDelay+Number(delay)}`);	// Fulfill
 			} else {
 				reject(`rejected promise ${position} in ${stepDelay}`);
-				console.log("reject step:", cnt, " ms:", cnt * stepDelay+Number(delay));
-				Notify.failure(`rejected promise ${cnt} in ${cnt * stepDelay+Number(delay)}`);	// Reject
+				//console.log("reject step:", cnt, " ms:", cnt * stepDelay+Number(delay));
+				Notify.failure(`rejected promise ${cnt+1} in ${cnt * stepDelay+Number(delay)}`);	// Reject
 			}
 			// console.log("step:", cnt," ms:", cnt*delay);
 			if (cnt > position-1) {
